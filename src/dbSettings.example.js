a593d846a33153b2787718b591867d058920b4dc
Mark Chambers <me@marksy.com>
2018-05-24 10:17:18 +1200

    example dbsettings

---
+++ src/dbSettings.example.js
@@ -0,0 +1,10 @@
+const dbSettings = {
+  apiKey: 'APIKEY',
+  authDomain: 'AUTHDOMAIN.FIREBASEAPP.COM',
+  databaseURL: 'HTTPS://DATABASEURL.FIREBASEIO.COM',
+  projectId: 'PROJECT-ID',
+  storageBucket: '',
+  messagingSenderId: 'MESSAGINGSENDERID'
+};
+
+export default dbSettings;
